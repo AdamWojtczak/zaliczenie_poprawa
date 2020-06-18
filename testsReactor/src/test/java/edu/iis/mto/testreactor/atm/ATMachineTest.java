6060576ca6ae23dfa11b5192cc1497f15a37a30d
AdamWojtczak <adamwojtczak807@gmail.com>
2020-06-18 18:27:57 +0200

    Hopeless commit
    Signed-off-by: AdamWojtczak <adamwojtczak807@gmail.com>

--- testsReactor/src/test/java/edu/iis/mto/testreactor/atm/ATMachineTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/atm/ATMachineTest.java
@@ -2,9 +2,11 @@ package edu.iis.mto.testreactor.atm;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 
 import edu.iis.mto.testreactor.atm.bank.AccountException;
 import edu.iis.mto.testreactor.atm.bank.AuthorizationException;
+import edu.iis.mto.testreactor.atm.bank.AuthorizationToken;
 import edu.iis.mto.testreactor.atm.bank.Bank;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,6 +15,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InOrder;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
@@ -27,14 +31,14 @@ public class ATMachineTest {
     @Mock
     private Card card;
     @Mock
-    private Money money;
-    @Mock
     private Bank bank;
+    @Mock
+    private Money money;
     private ATMachine atMachine;
 
     @BeforeEach
     public void setUp() throws Exception {
-//        Mockito.when(money.getCurrency()).thenReturn(Currency.getInstance("PLN"));
+        Mockito.when(money.getCurrency()).thenReturn(Currency.getInstance("PLN"));
         atMachine = new ATMachine(bank, money.getCurrency());
     }
 
@@ -56,7 +60,13 @@ public class ATMachineTest {
         callorder.verify(bank).autorize(pinCode.getPIN(), card.getNumber());
         callorder.verify(money).getDenomination();
         callorder.verify(money).getCurrency();
-        callorder.verify(bank).charge(Mockito.any(), money);
+        callorder.verify(bank).charge(any(), money);
+    }
+
+    @Test
+    public void catchinSomeExceptions() throws ATMOperationException {
+        Mockito.doThrow(ATMOperationException.class).validateAmount(money);
+        Withdrawal withdrawal = atMachine.withdraw(pinCode, card, money);
     }
 
     private Withdrawal success(PinCode pin, Card card, Money amount) {
@@ -65,8 +75,11 @@ public class ATMachineTest {
         for (Banknote banknote : Banknote.getDescFor(amount.getCurrency())) {
             banknotesPackList.add(BanknotesPack.create(10, banknote));
         }
-
         return Withdrawal.create(banknotesPackList);
+    }
+
+    private Withdrawal failure(PinCode pin, Card card, Money amount) {
+
     }
 
     @Test
