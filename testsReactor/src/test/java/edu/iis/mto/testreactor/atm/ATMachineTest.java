373d8cc3b628950be4e1ee8720c683fd3b7f6ffa
AdamWojtczak <adamwojtczak807@gmail.com>
2020-06-18 18:04:24 +0200

    Behavioral done - not working State - NullPointerException
    Signed-off-by: AdamWojtczak <adamwojtczak807@gmail.com>

--- testsReactor/src/test/java/edu/iis/mto/testreactor/atm/ATMachineTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/atm/ATMachineTest.java
@@ -1,15 +1,73 @@
 package edu.iis.mto.testreactor.atm;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import edu.iis.mto.testreactor.atm.bank.AccountException;
+import edu.iis.mto.testreactor.atm.bank.AuthorizationException;
+import edu.iis.mto.testreactor.atm.bank.Bank;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InOrder;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+import java.util.Currency;
+import java.util.List;
+
+@ExtendWith(MockitoExtension.class)
 public class ATMachineTest {
 
+    @Mock
+    private PinCode pinCode;
+    @Mock
+    private Card card;
+    @Mock
+    private Money money;
+    @Mock
+    private Bank bank;
+    private ATMachine atMachine;
+
     @BeforeEach
-    public void setUp() throws Exception {}
+    public void setUp() throws Exception {
+//        Mockito.when(money.getCurrency()).thenReturn(Currency.getInstance("PLN"));
+        atMachine = new ATMachine(bank, money.getCurrency());
+    }
+
+    @Test
+    public void successfullWithdrawal() throws ATMOperationException {
+        Withdrawal withdrawal = atMachine.withdraw(pinCode, card, money);
+        assertEquals(success(pinCode, card, money), withdrawal);
+    }
+
+    @Test
+    public void behavioralTest() throws ATMOperationException, AuthorizationException, AccountException {
+        Withdrawal withdrawal = atMachine.withdraw(pinCode, card, money);
+
+        InOrder callorder = Mockito.inOrder(money, pinCode);
+
+        callorder.verify(money).getCurrency();
+        callorder.verify(pinCode).getPIN();
+        callorder.verify(card).getNumber();
+        callorder.verify(bank).autorize(pinCode.getPIN(), card.getNumber());
+        callorder.verify(money).getDenomination();
+        callorder.verify(money).getCurrency();
+        callorder.verify(bank).charge(Mockito.any(), money);
+    }
+
+    private Withdrawal success(PinCode pin, Card card, Money amount) {
+        int notImportant = 10;
+        List<BanknotesPack> banknotesPackList = new ArrayList<>();
+        for (Banknote banknote : Banknote.getDescFor(amount.getCurrency())) {
+            banknotesPackList.add(BanknotesPack.create(10, banknote));
+        }
+
+        return Withdrawal.create(banknotesPackList);
+    }
 
     @Test
     public void itCompiles() {
