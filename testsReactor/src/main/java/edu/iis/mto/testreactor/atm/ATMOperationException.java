8606114e978565e4c3740c35e6c79c438f557d70
AdamWojtczak <adamwojtczak807@gmail.com>
2020-09-12 10:38:04 +0200

    Hopeless commit v2
    Signed-off-by: AdamWojtczak <adamwojtczak807@gmail.com>

--- testsReactor/src/main/java/edu/iis/mto/testreactor/atm/ATMOperationException.java
+++ testsReactor/src/main/java/edu/iis/mto/testreactor/atm/ATMOperationException.java
@@ -1,10 +1,25 @@
 package edu.iis.mto.testreactor.atm;
 
+import java.util.Objects;
+
 public class ATMOperationException extends Exception {
 
     private static final long serialVersionUID = 1L;
 
     private final ErrorCode errorCode;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ATMOperationException that = (ATMOperationException) o;
+        return errorCode == that.errorCode;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(errorCode);
+    }
 
     public ATMOperationException(ErrorCode errorCode) {
         this.errorCode = errorCode;
--- testsReactor/src/test/java/edu/iis/mto/testreactor/atm/ATMachineTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/atm/ATMachineTest.java
@@ -2,6 +2,7 @@ package edu.iis.mto.testreactor.atm;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 
 import edu.iis.mto.testreactor.atm.bank.AccountException;
@@ -15,8 +16,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InOrder;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
-import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
@@ -26,62 +25,99 @@ import java.util.List;
 @ExtendWith(MockitoExtension.class)
 public class ATMachineTest {
 
-    @Mock
-    private PinCode pinCode;
-    @Mock
-    private Card card;
-    @Mock
-    private Bank bank;
-    @Mock
-    private Money money;
-    private ATMachine atMachine;
+	@Mock
+	private Bank bank;
+	private PinCode pinCode;
+	private Card card;
+	private Currency currency = Currency.getInstance("PLN");
+	private Money money;
+	private ATMachine atMachine;
 
-    @BeforeEach
-    public void setUp() throws Exception {
-        Mockito.when(money.getCurrency()).thenReturn(Currency.getInstance("PLN"));
-        atMachine = new ATMachine(bank, money.getCurrency());
-    }
+	@BeforeEach
+	public void setUp() throws Exception {
+		money = new Money(0);
+		card = Card.create("1234");
+		pinCode = PinCode.createPIN(1, 2, 3, 4);
+//		Mockito.when(pinCode.getPIN()).thenReturn("1234");
+		atMachine = new ATMachine(bank, currency);
+	}
 
-    @Test
-    public void successfullWithdrawal() throws ATMOperationException {
-        Withdrawal withdrawal = atMachine.withdraw(pinCode, card, money);
-        assertEquals(success(pinCode, card, money), withdrawal);
-    }
+	@Test
+	public void successfullWithdrawal() throws ATMOperationException, AuthorizationException {
+		BanknotesPack banknotesPack = BanknotesPack.create(50, Banknote.PL_10);
+		List<BanknotesPack> banknotesPackList = new ArrayList<>();
+		banknotesPackList.add(banknotesPack);
 
-    @Test
-    public void behavioralTest() throws ATMOperationException, AuthorizationException, AccountException {
-        Withdrawal withdrawal = atMachine.withdraw(pinCode, card, money);
+		Mockito.when(bank.autorize("1234", "1234")).thenReturn(AuthorizationToken.create("1234"));
+		Withdrawal withdrawal = atMachine.withdraw(pinCode, card, money);
 
-        InOrder callorder = Mockito.inOrder(money, pinCode);
+		Withdrawal expectedWithdrawal = getSuccessfulWithdrawal(money);
 
-        callorder.verify(money).getCurrency();
-        callorder.verify(pinCode).getPIN();
-        callorder.verify(card).getNumber();
-        callorder.verify(bank).autorize(pinCode.getPIN(), card.getNumber());
-        callorder.verify(money).getDenomination();
-        callorder.verify(money).getCurrency();
-        callorder.verify(bank).charge(any(), money);
-    }
+		assertEquals(expectedWithdrawal, withdrawal);
+	}
 
-    @Test
-    public void catchinSomeExceptions() throws ATMOperationException {
-//        Mockito.doThrow(ATMOperationException.class).validateAmount(money);
-        Withdrawal withdrawal = atMachine.withdraw(pinCode, card, money);
-    }
+	private Withdrawal getSuccessfulWithdrawal(Money money) throws ATMOperationException {
+		BanknotesPack banknotesPack;
+		List<BanknotesPack> banknotesPackList;
+		banknotesPack = BanknotesPack.create(0, Banknote.PL_10);
+		banknotesPackList = new ArrayList<>();
+		banknotesPackList.add(banknotesPack);
 
-    private Withdrawal success(PinCode pin, Card card, Money amount) {
-        int notImportant = 10;
-        List<BanknotesPack> banknotesPackList = new ArrayList<>();
-        for (Banknote banknote : Banknote.getDescFor(amount.getCurrency())) {
-            banknotesPackList.add(BanknotesPack.create(10, banknote));
-        }
-        return Withdrawal.create(banknotesPackList);
-    }
+		Withdrawal withdrawal = atMachine.withdraw(pinCode, card, money);
+		return withdrawal;
+	}
+
+	@Test
+	public void behavioralTest() throws ATMOperationException, AuthorizationException, AccountException {
+
+		Mockito.when(bank.autorize("1234", "1234")).thenReturn(AuthorizationToken.create("1234"));
+		getSuccessfulWithdrawal(money);
+
+		InOrder callorder = Mockito.inOrder(bank);
+
+//		callorder.verify(money).getCurrency();
+//		callorder.verify(pinCode).getPIN();
+//		callorder.verify(card).getNumber();
+		callorder.verify(bank).autorize(pinCode.getPIN(), card.getNumber());
+//		callorder.verify(money).getDenomination();
+//		callorder.verify(money).getCurrency();
+		callorder.verify(bank).charge(AuthorizationToken.create("1234"), money);
+	}
+
+	@Test
+	public void catchinSomeExceptions() throws ATMOperationException, AuthorizationException {
+//		try {
+			Mockito.doThrow(AuthorizationException.class).when(bank).autorize(Mockito.any(), Mockito.any());
+//			atMachine.withdraw(pinCode, card, money);
+//		} catch (Exception e) {
+//			assertEquals(new ATMOperationException(ErrorCode.AHTHORIZATION), e);
+//		}
+		assertThrows(new ATMOperationException(ErrorCode.AHTHORIZATION).getClass(), () -> atMachine.withdraw(pinCode, card, money));
+	}
+
+	@Test
+	public void catchinSomeOtherExceptions() throws AccountException {
+		try {
+			Mockito.doThrow(AccountException.class).when(bank).charge(Mockito.any(), Mockito.any());
+			atMachine.withdraw(pinCode, card, money);
+		} catch (Exception e) {
+			assertEquals(new ATMOperationException(ErrorCode.NO_FUNDS_ON_ACCOUNT), e);
+		}
+	}
+
+	@Test
+	public void catchinSomeOther2Exceptions() throws ATMOperationException {
+		try {
+			getSuccessfulWithdrawal(new Money(20));
+		} catch (Exception e) {
+			assertEquals(new ATMOperationException(ErrorCode.WRONG_AMOUNT), e);
+		}
+	}
 
 
-    @Test
-    public void itCompiles() {
-        assertThat(true, Matchers.equalTo(true));
-    }
+	@Test
+	public void itCompiles() {
+		assertThat(true, Matchers.equalTo(true));
+	}
 
 }
